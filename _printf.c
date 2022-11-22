580dbb1ca0fc9b9bf69a94dc34174768641caa55
Daniel John <dj71286@gmail.com>
2022-11-22 01:07:41 +0000

    fixed hopefully

--- _printf.c
+++ _printf.c
@@ -25,7 +25,7 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	A
+
 
 Here:
 	while (format[i] != '\0')
