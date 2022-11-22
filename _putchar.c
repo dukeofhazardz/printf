0ba6dde34565ea00ce8287397bf16c77b5c8ec18
Daniel John <dj71286@gmail.com>
2022-11-22 00:12:26 +0000

    Another commit

--- _putchar.c
+++ _putchar.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _putchar - A function that prints a character
+ * @c: Character to be printed
+ * Return: Integer
+ */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
--- convert.c
+++ convert.c
@@ -1,17 +1,23 @@
 #include "main.h"
 
+/**
+ * convert - A function that converts bases
+ * @n: a character arguement
+ * @base: base to be converted to
+ * Return: a pointer
+ */
+
 char *convert(unsigned int n, int base)
 {
-	static char Representation[] = "0123456789abcdef";
+	static const char Representation[] = "0123456789abcdef";
 	static char buffer[50];
 	char *ptr;
 
 	ptr = &buffer[49];
 	*ptr = '\0';
 
-	do
-	{
-		*--ptr = Representation[n%base];
+	do {
+		*--ptr = Representation[n % base];
 		n /= base;
 	} while (n != 0);
 
