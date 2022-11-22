486985cbe4fb9154cb6700f98db4112f988554b0
Philemon Okpokpa <113598609+sirphilalx@users.noreply.github.com>
2022-11-22 01:02:50 +0100

    added the description for the function

--- convert_HEX.c
+++ convert_HEX.c
@@ -1,17 +1,24 @@
 #include "main.h"
 
+/**
+* convert_HEX - converts arguments
+* @n: the first argument
+* @base: the second argument
+*
+* Return: returns a pointer
+*/
+
 char *convert_HEX(unisigned int n, int base)
 {
-	static char Representation[] = "0123456789ABCDEF";
+	static const char Representation[] = "0123456789ABCDEF";
 	static char buffer[50];
 	char *ptr;
 
 	ptr = &buffer[49];
 	*ptr = '\0';
 
-	do
-	{
-		*--ptr = Representation[n%base];
+	do {
+		*--ptr = Representation[n % base];
 		n /= base;
 	} while (n != 0);
 
