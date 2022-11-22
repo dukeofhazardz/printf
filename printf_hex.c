46a4721740e194146e1153caec2a9127dcdc6cd8
Daniel John <dj71286@gmail.com>
2022-11-22 00:55:21 +0000

    fix

--- printf_hex.c
+++ printf_hex.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * printf_HEX - A function that coverts to Hexadecimal
+ * printf_hex - A function that coverts to Hexadecimal
  * @arg: a character arguement
  * Return: Integer.
  */
 
-int printf_HEX(va_list arg)
+int printf_hex(va_list arg)
 {
 	unsigned int i, n;
 
 	n = va_arg(arg, unsigned int);
-	i = _puts(convert_HEX(n, 16));
+	i = _puts(convert(n, 16));
 	return (i);
 }
